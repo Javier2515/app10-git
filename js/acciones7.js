e58d23f2415814c549d9ec71fdc424160220512a
Javier2515 <javier.leon.rangel.cbtis16@gmail.com>
2017-05-31 23:25:36 -0500

    1subida

---
+++ js/acciones7.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
+
+
+$('#beep').tap(function(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+			
+			case 2:
+			break;
+		}
+	},"Aplicaion7","Aceptar,Cancelar");
+	
+});
+	},false);
+});
