ee7752d1b2c96b818daacc8b4185deab38f90d43
Javier2515 <javier.leon.rangel.cbtis16@gmail.com>
2017-06-02 10:41:48 -0500

    25

--- js/acciones7.js
+++ js/acciones7.js
@@ -3,22 +3,38 @@ document.addEventListener("deviceready", loaded, false);
 function loaded(){
 alert("device is ready");
 };
-  function MamWorkDone(){
+  function tierra1(){
 	navigator.notification.confirm("Se abrira el navegador",function(opt){
 		switch(opt)
 		{
-			
-			
 			case 1:
-			var ref = cordova.InAppBrowser.open('http://html-color-codes.info', '_system', 'location=yes');
-			break;
-			
-		
-			
-			case 2:
-			var ref = cordova.InAppBrowser.open('http://html-color-codes.info', '_blank', 'location=yes');
+			var ref = cordova.InAppBrowser.open('https://es.wikipedia.org/wiki/Día_de_la_Tierra', '_system', 'location=yes');
 			break;
 		}
-	},"Aplicaion7","System,Blank,Cancelar");
+	},"Advertencia","Continuar,Cancelar");
   };
+  
 
+  function tierra2(){
+	navigator.notification.confirm("Se abrira el navegador",function(opt){
+		switch(opt)
+		{
+			case 1:
+			var ref = cordova.InAppBrowser.open('http://www.un.org/es/events/motherearthday/', '_system', 'location=yes');
+			break;
+		}
+	},"Advertencia","Continuar,Cancelar");
+  };
+  
+  
+  
+  function tierra3(){
+	navigator.notification.confirm("Se abrira el navegador",function(opt){
+		switch(opt)
+		{
+			case 1:
+			var ref = cordova.InAppBrowser.open('http://www.nationalgeographic.es/medio-ambiente/datos-del-dia-de-la-tierra-cuando-es-como-empezo', '_system', 'location=yes');
+			break;
+		}
+	},"Advertencia","Continuar,Cancelar");
+  };
