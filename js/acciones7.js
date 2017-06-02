66f457a4995ff256e266a2b7a3b41034bd62aeaa
Javier2515 <javier.leon.rangel.cbtis16@gmail.com>
2017-06-01 19:54:31 -0500

    9

--- js/acciones7.js
+++ js/acciones7.js
@@ -1,45 +1,20 @@
-$(document).ready(function(e) {
-    document.addEventListener("deviceready",function(){
-		
-		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion7","Aceptar"});
-          });
-
-$('#beep').tap(function(){
-	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
-		switch(opt)
-		{
-			case 1:
-			navigator.notification.beep(2);
-			break;
-			
-			case 2:
-			navigator.notification.vibrate(1000);
-			break;
-		}
-	},"Aplicaion7","Beep,Vibrar,Cancelar");
-	
-});
-
-
-
-
-function lol(){
-	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
-		switch(opt)
-		{
-			case 1:
-			navigator.notification.beep(2);
-			break;
-			
-			case 2:
-			navigator.notification.vibrate(1000);
-			break;
-		}
-	},"Aplicaion7","Beep,Vibrar,Cancelar");
-	
+// JavaScript Document<script>
+document.addEventListener("deviceready", loaded, false);
+function loaded(){
+alert("device is ready");
 };
+  function MamWorkDone(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(2);
+			break;
+			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"Aplicaion7","Beep,Vibrar,Cancelar");
+  };
 
-
-	},false);
-});
